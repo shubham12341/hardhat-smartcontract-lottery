9a7aa9f97144e871e00f7b59421f15a4ad2ed5ab
Shubham Gangurde <shubham786313@gmail.com>
2024-02-27 16:58:19 +0530

    Delete tp.js

--- tp.js
+++
@@ -1,5 +0,0 @@
-// Get our subId for Chainlink VRF
-// Deploy our contract using subId
-// Register the contract with Chainlink VRF and its subId
-// Register the contract with Chainlink Automation
-// Run Staging tests
